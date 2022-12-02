e540d9e440228a24bec20defefd9bbd5790d23f0
SahiraTejada <Sahira2208@hotmail.com>
2022-12-02 09:56:38 -0400

    half done program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,19 @@
-﻿
-using Antlr4.Runtime.Tree;
+﻿using Antlr4.Runtime.Tree;
 using Antlr4.Runtime;
 using Composicion;
 using Antlr4.Runtime.Misc;
 
-var textFile = CharStreams.fromPath(@"..\..\..\ejemplo.txt");
+string textFile = @"..\..\..\ejemplo.txt";
 
-Console.WriteLine(textFile);
+Console.WriteLine("");
+Console.WriteLine("\tLenguaje de composición de textos\n");
 
+Console.WriteLine("Verificando el texto... \n");
 
 IParseTree parse(string textFile)
 {
     ImmediateErrorListener errListener = ImmediateErrorListener.Instance;
-    var inputStream = CharStreams.fromString(textFile);
+    var inputStream = CharStreams.fromPath(textFile);
     var lexer = new ComposicionLexer(inputStream);
     var tokenStream = new CommonTokenStream(lexer);
     var parser = new ComposicionParser(tokenStream);
@@ -21,7 +22,39 @@ IParseTree parse(string textFile)
     var tree = parser.textComp();
     return tree;
 }
+
+
+bool ok = true;
 IParseTree? tree = null;
+string input;
+int count = 0;
+
+do
+{
+    input = textFile;
+    try
+    {
+        tree = parse(input);
+        ok = true;
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Documento de word ha sido generado exitosamente!!");
+        Console.ResetColor();
+
+    }
+    catch (ParseCanceledException e)
+    {
+        count++;
+        Console.WriteLine("No se ha podido generar el documento de word debido a errores\n");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(e.Message);
+        Console.ResetColor();
+        Console.WriteLine($"{count}");
+        
+
+    }
+
+} while (!ok);
+
 var composicion = new Composicion.Composicion();
 
 
